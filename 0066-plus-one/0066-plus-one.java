20b4c1fa45c043695fceaabf2ef88c5dba714e66
Yogendra Singh <iamysy7@gmail.com>
2024-09-03 22:36:45 +0530

    Time: 0 ms (100%), Space: 41.5 MB (94.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -3,10 +3,10 @@ class Solution {
         for(int i=digits.length-1;i>=0;i--){
             digits[i] +=1;
             if (digits[i]<10){
-                return digits;
+             return digits;
             }
             digits[i]=0;
-        }
+        }               
         int[] result =new int[digits.length+1];
         result[0]=1;
         return result;
